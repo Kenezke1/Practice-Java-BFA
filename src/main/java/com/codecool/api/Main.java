60dcadd5623adf08c47c24ea16721b983daf1e2b
Kenezke1 <herczeg.kenya2@gmail.com>
2018-02-25 17:44:28 +0100

    MÁTÉNAK

--- src/main/java/com/codecool/api/Main.java
+++ src/main/java/com/codecool/api/Main.java
@@ -3,7 +3,7 @@ package com.codecool.api;
 public class Main {
 
     public static void main(String[] args) {
-        ClothesMenager cm = new ClothesMenager();
+        ClothesManager cm = new ClothesManager();
 
         for (Clothes c : cm.loadClothes()) {
             System.out.println(c.toString());
